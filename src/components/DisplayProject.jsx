e81c04b863a248f8d1eded32ee821357dc6dee31
gabrieltdrk <gabriel.andradesx@gmail.com>
2024-11-14 16:41:46 -0300

    added project click

--- src/components/DisplayProject.jsx
+++ src/components/DisplayProject.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom'
+
 export function DisplayProject({ project }) {
+  const navigate = useNavigate()
+
+  function handleProjectClick() {
+    navigate(`/projects/${project.id}`)
+  }
   return (
-    <div>
-      <h1>{project.id}</h1>
-      <h2>{project.title}</h2>
-      <h2>{project.languages}</h2>
+    <div onClick={handleProjectClick} className="bg-slate-100 justify-center rounded-md m-3 hover:cursor-pointer">
+      <div className="flex flex-col gap-6 p-6 items-center">
+        <h2>{project.title}</h2>
+        <h2>{project.languages}</h2>
+      </div>
     </div>
   )
 }
--- src/components/DisplayProjects.jsx
+++ src/components/DisplayProjects.jsx
@@ -2,16 +2,12 @@ import json from '../projects.json'
 import { DisplayProject } from './DisplayProject'
 
 export function DisplayProjects({ isHome }) {
-    const slicedProjects = isHome ? json.projects.slice(0,3) : json.projects
+  const slicedProjects = isHome ? json.projects.slice(0, 3) : json.projects
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 justify-center items-center bg-slate-300">
-      <div className="p-4">
-        <div className="mb-6">
-          {slicedProjects.map((project) => (
-            <DisplayProject project={project} key={project.id} />
-          ))}
-        </div>
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-3 justify-center items-center bg-slate-300 p-6">
+      {slicedProjects.map((project) => (
+        <DisplayProject project={project} key={project.id} />
+      ))}
     </div>
   )
 }
