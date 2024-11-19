2520a6d3275991cfa33393b29ae50070207a75b4
gabrieltdrk <gabriel.andradesx@gmail.com>
2024-11-18 21:52:10 -0300

    minor fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export function Footer() {
       <footer className={`md:text-2xl p-4 w-full bg-default-dark text-zinc-100 transition-all`}>
         <div className="flex justify-between w-[60%] mx-auto">
           <h1 className="self-center">Gabriel Andrade | © {year} </h1>
-          <div className="grid grid-cols-4 items-center gap-4">
+          <div className="grid grid-cols-3 items-center gap-4">
             <Link to="https://github.com/gabrieltdrk"><GithubLogo /></Link>
             <Link to="https://www.linkedin.com/in/gabrieltdrk/"><LinkedinLogo /></Link>
             <Link to="https://x.com/gabrieltdrk"><XLogo /></Link>
